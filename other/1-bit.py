35d8b77ba68f232ef2814ad5cc40c0eae8054ee4
RainbowLegend <rainbowlegend3054@gmail.com>
2018-11-20 22:43:06 -0500

    1 BIT!!!!

---
+++ other/1-bit.py
@@ -0,0 +1,10 @@
+print('Welcome to my first converter!!!!')
+num = input('Whats your number???')
+
+if '0' == num:
+    print('The number is 0!!!')
+
+if '1' == num:
+    print('The number is 1!!!')
+
+print('Thanks for using my calculator!!!!')
