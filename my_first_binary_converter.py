f99f6f858f29cddc243eb0afe53ad4bd79374ac6
RainbowLegend <rainbowlegend3054@gmail.com>
2018-11-20 21:46:07 -0500

    Hi world!!!
    HELLO!!! THIS IS AN 8 BIT CONVERTER!!!

---
+++ my_first_binary_converter.py
@@ -0,0 +1,772 @@
+print('Welcome to my first converter!!!!')
+num = input('Whats your number???')
+
+if '00000000' == num:
+    print('The number is 0!!!')
+
+if '00000001' == num:
+    print('The number is 1!!!')
+
+if '00000010' == num:
+    print('The number is 2!!!')
+
+if '00000011' == num:
+    print('The number is 3!!!')
+
+if '00000100' == num:
+    print('The number is 4!!!')
+
+if '00000101' == num:
+    print('The number is 5!!!')
+
+if '00000110' == num:
+    print('The number is 6!!!')
+
+if '00000111' == num:
+    print('The number is 7!!!')
+
+if '00001000' == num:
+    print('The number is 8!!!')
+
+if '00001001' == num:
+    print('The number is 9!!!')
+
+if '00001010' == num:
+    print('The number is 10!!!')
+
+if '00001011' == num:
+    print('The number is 11!!!')
+
+if '00001100' == num:
+    print('The number is 12!!!')
+
+if '00001101' == num:
+    print('The number is 13!!!')
+
+if '00001110' == num:
+    print('The number is 14!!!')
+
+if '00001111' == num:
+    print('The number is 15!!!')
+
+if '00010000' == num:
+    print('The number is 16!!!')
+
+if '00010001' == num:
+    print('The number is 17!!!')
+
+if '00010010' == num:
+    print('The number is 18!!!')
+
+if '00010011' == num:
+    print('The number is 19!!!')
+
+if '00010100' == num:
+    print('The number is 20!!!')
+
+if '00010101' == num:
+    print('The number is 21!!!')
+
+if '00010110' == num:
+    print('The number is 22!!!')
+
+if '00010111' == num:
+    print('The number is 23!!!')
+
+if '00011000' == num:
+    print('The number is 24!!!')
+
+if '00011001' == num:
+    print('The number is 25!!!')
+
+if '00011010' == num:
+    print('The number is 26!!!')
+
+if '00011011' == num:
+    print('The number is 27!!!')
+
+if '00011100' == num:
+    print('The number is 28!!!')
+
+if '00011101' == num:
+    print('The number is 29!!!')
+
+if '00011110' == num:
+    print('The number is 30!!!')
+
+if '00011111' == num:
+    print('The number is 31!!!')
+
+if '00100000' == num:
+    print('The number is 32!!!')
+
+if '00100001' == num:
+    print('The number is 33!!!')
+
+if '00100010' == num:
+    print('The number is 34!!!')
+
+if '00100011' == num:
+    print('The number is 35!!!')
+
+if '00100100' == num:
+    print('The number is 36!!!')
+
+if '00100101' == num:
+    print('The number is 37!!!')
+
+if '00100110' == num:
+    print('The number is 38!!!')
+
+if '00100111' == num:
+    print('The number is 39!!!')
+
+if '00101000' == num:
+    print('The number is 40!!!')
+
+if '00101001' == num:
+    print('The number is 41!!!')
+
+if '00101010' == num:
+    print('The number is 42!!!')
+
+if '00101011' == num:
+    print('The number is 43!!!')
+
+if '00101100' == num:
+    print('The number is 44!!!')
+
+if '00101101' == num:
+    print('The number is 45!!!')
+
+if '00101110' == num:
+    print('The number is 46!!!')
+
+if '00101111' == num:
+    print('The number is 47!!!')
+
+if '00110000' == num:
+    print('The number is 48!!!')
+
+if '00110001' == num:
+    print('The number is 49!!!')
+
+if '00110010' == num:
+    print('The number is 50!!!')
+
+if '00110011' == num:
+    print('The number is 51!!!')
+
+if '00110100' == num:
+    print('The number is 52!!!')
+
+if '00110101' == num:
+    print('The number is 53!!!')
+
+if '00110110' == num:
+    print('The number is 54!!!')
+
+if '00110111' == num:
+    print('The number is 55!!!')
+
+if '00111000' == num:
+    print('The number is 56!!!')
+
+if '00111001' == num:
+    print('The number is 57!!!')
+
+if '00111010' == num:
+    print('The number is 58!!!')
+
+if '00111011' == num:
+    print('The number is 59!!!')
+
+if '00111100' == num:
+    print('The number is 60!!!')
+
+if '00111101' == num:
+    print('The number is 61!!!')
+
+if '00111110' == num:
+    print('The number is 62!!!')
+
+if '00111111' == num:
+    print('The number is 63!!!')
+
+if '01000000' == num:
+    print('The number is 64!!!')
+
+if '01000001' == num:
+    print('The number is 65!!!')
+
+if '01000010' == num:
+    print('The number is 66!!!')
+
+if '01000011' == num:
+    print('The number is 67!!!')
+
+if '01000100' == num:
+    print('The number is 68!!!')
+
+if '01000101' == num:
+    print('The number is 69!!!')
+
+if '01000110' == num:
+    print('The number is 70!!!')
+
+if '01000111' == num:
+    print('The number is 71!!!')
+
+if '01001000' == num:
+    print('The number is 72!!!')
+
+if '01001001' == num:
+    print('The number is 73!!!')
+
+if '01001010' == num:
+    print('The number is 74!!!')
+
+if '01001011' == num:
+    print('The number is 75!!!')
+
+if '01001100' == num:
+    print('The number is 76!!!')
+
+if '01001101' == num:
+    print('The number is 77!!!')
+
+if '01001110' == num:
+    print('The number is 78!!!')
+
+if '01001111' == num:
+    print('The number is 79!!!')
+
+if '01010000' == num:
+    print('The number is 80!!!')
+
+if '01010001' == num:
+    print('The number is 81!!!')
+
+if '01010010' == num:
+    print('The number is 82!!!')
+
+if '01010011' == num:
+    print('The number is 83!!!')
+
+if '01010100' == num:
+    print('The number is 84!!!')
+
+if '01010101' == num:
+    print('The number is 85!!!')
+
+if '01010110' == num:
+    print('The number is 86!!!')
+
+if '01010111' == num:
+    print('The number is 87!!!')
+
+if '01011000' == num:
+    print('The number is 88!!!')
+
+if '01011001' == num:
+    print('The number is 89!!!')
+
+if '01011010' == num:
+    print('The number is 90!!!')
+
+if '01011011' == num:
+    print('The number is 91!!!')
+
+if '01011100' == num:
+    print('The number is 92!!!')
+
+if '01011101' == num:
+    print('The number is 93!!!')
+
+if '01011110' == num:
+    print('The number is 94!!!')
+
+if '01011111' == num:
+    print('The number is 95!!!')
+
+if '01100000' == num:
+    print('The number is 96!!!')
+
+if '01100001' == num:
+    print('The number is 97!!!')
+
+if '01100010' == num:
+    print('The number is 98!!!')
+
+if '01100011' == num:
+    print('The number is 99!!!')
+
+if '01100100' == num:
+    print('The number is 100!!!')
+
+if '01100101' == num:
+    print('The number is 101!!!')
+
+if '01100110' == num:
+    print('The number is 102!!!')
+
+if '01100111' == num:
+    print('The number is 103!!!')
+
+if '01101000' == num:
+    print('The number is 104!!!')
+
+if '01101001' == num:
+    print('The number is 105!!!')
+
+if '01101010' == num:
+    print('The number is 106!!!')
+
+if '01101011' == num:
+    print('The number is 107!!!')
+
+if '01101100' == num:
+    print('The number is 108!!!')
+
+if '01101101' == num:
+    print('The number is 109!!!')
+
+if '01101110' == num:
+    print('The number is 110!!!')
+
+if '01101111' == num:
+    print('The number is 111!!!')
+
+if '01110000' == num:
+    print('The number is 112!!!')
+
+if '01110001' == num:
+    print('The number is 113!!!')
+
+if '01110010' == num:
+    print('The number is 114!!!')
+
+if '01110011' == num:
+    print('The number is 115!!!')
+
+if '01110100' == num:
+    print('The number is 116!!!')
+
+if '01110101' == num:
+    print('The number is 117!!!')
+
+if '01110110' == num:
+    print('The number is 118!!!')
+
+if '01110111' == num:
+    print('The number is 119!!!')
+
+if '01111000' == num:
+    print('The number is 120!!!')
+
+if '01111001' == num:
+    print('The number is 121!!!')
+
+if '01111010' == num:
+    print('The number is 122!!!')
+
+if '01111011' == num:
+    print('The number is 123!!!')
+
+if '01111100' == num:
+    print('The number is 124!!!')
+
+if '01111101' == num:
+    print('The number is 125!!!')
+
+if '01111110' == num:
+    print('The number is 126!!!')
+
+if '01111111' == num:
+    print('The number is 127!!!')
+
+if '10000000' == num:
+    print('The number is 128!!!')
+
+if '10000001' == num:
+    print('The number is 129!!!')
+
+if '10000010' == num:
+    print('The number is 130!!!')
+
+if '10000011' == num:
+    print('The number is 131!!!')
+
+if '10000100' == num:
+    print('The number is 132!!!')
+
+if '10000101' == num:
+    print('The number is 133!!!')
+
+if '10000110' == num:
+    print('The number is 134!!!')
+
+if '10000111' == num:
+    print('The number is 135!!!')
+
+if '10001000' == num:
+    print('The number is 136!!!')
+
+if '10001001' == num:
+    print('The number is 137!!!')
+
+if '10001010' == num:
+    print('The number is 138!!!')
+
+if '10001011' == num:
+    print('The number is 139!!!')
+
+if '10001100' == num:
+    print('The number is 140!!!')
+
+if '10001101' == num:
+    print('The number is 141!!!')
+
+if '10001110' == num:
+    print('The number is 142!!!')
+
+if '10001111' == num:
+    print('The number is 143!!!')
+
+if '10010000' == num:
+    print('The number is 144!!!')
+
+if '10010001' == num:
+    print('The number is 145!!!')
+
+if '10010010' == num:
+    print('The number is 146!!!')
+
+if '10010011' == num:
+    print('The number is 147!!!')
+
+if '10010100' == num:
+    print('The number is 148!!!')
+
+if '10010101' == num:
+    print('The number is 149!!!')
+
+if '10010110' == num:
+    print('The number is 150!!!')
+
+if '10010111' == num:
+    print('The number is 151!!!')
+
+if '10011000' == num:
+    print('The number is 152!!!')
+
+if '10011001' == num:
+    print('The number is 153!!!')
+
+if '10011010' == num:
+    print('The number is 154!!!')
+
+if '10011011' == num:
+    print('The number is 155!!!')
+
+if '10011100' == num:
+    print('The number is 156!!!')
+
+if '10011101' == num:
+    print('The number is 157!!!')
+
+if '10011110' == num:
+    print('The number is 158!!!')
+
+if '10011111' == num:
+    print('The number is 159!!!')
+
+if '10100000' == num:
+    print('The number is 160!!!')
+
+if '10100001' == num:
+    print('The number is 161!!!')
+
+if '10100010' == num:
+    print('The number is 162!!!')
+
+if '10100011' == num:
+    print('The number is 163!!!')
+
+if '10100100' == num:
+    print('The number is 164!!!')
+
+if '10100101' == num:
+    print('The number is 165!!!')
+
+if '10100110' == num:
+    print('The number is 166!!!')
+
+if '10100111' == num:
+    print('The number is 167!!!')
+
+if '10101000' == num:
+    print('The number is 168!!!')
+
+if '10101001' == num:
+    print('The number is 169!!!')
+
+if '10101010' == num:
+    print('The number is 170!!!')
+
+if '10101011' == num:
+    print('The number is 171!!!')
+
+if '10101100' == num:
+    print('The number is 172!!!')
+
+if '10101101' == num:
+    print('The number is 173!!!')
+
+if '10101110' == num:
+    print('The number is 174!!!')
+
+if '10101111' == num:
+    print('The number is 175!!!')
+
+if '10110000' == num:
+    print('The number is 176!!!')
+
+if '10110001' == num:
+    print('The number is 177!!!')
+
+if '10110010' == num:
+    print('The number is 178!!!')
+
+if '10110011' == num:
+    print('The number is 179!!!')
+
+if '10110100' == num:
+    print('The number is 180!!!')
+
+if '10110101' == num:
+    print('The number is 181!!!')
+
+if '10110110' == num:
+    print('The number is 182!!!')
+
+if '10110111' == num:
+    print('The number is 183!!!')
+
+if '10111000' == num:
+    print('The number is 184!!!')
+
+if '10111001' == num:
+    print('The number is 185!!!')
+
+if '10111010' == num:
+    print('The number is 186!!!')
+
+if '10111011' == num:
+    print('The number is 187!!!')
+
+if '10111100' == num:
+    print('The number is 188!!!')
+
+if '10111101' == num:
+    print('The number is 189!!!')
+
+if '10111110' == num:
+    print('The number is 190!!!')
+
+if '10111111' == num:
+    print('The number is 191!!!')
+
+if '11000000' == num:
+    print('The number is 192!!!')
+
+if '11000001' == num:
+    print('The number is 193!!!')
+
+if '11000010' == num:
+    print('The number is 194!!!')
+
+if '11000011' == num:
+    print('The number is 195!!!')
+
+if '11000100' == num:
+    print('The number is 196!!!')
+
+if '11000101' == num:
+    print('The number is 197!!!')
+
+if '11000110' == num:
+    print('The number is 198!!!')
+
+if '11000111' == num:
+    print('The number is 199!!!')
+
+if '11001000' == num:
+    print('The number is 200!!!')
+
+if '11001001' == num:
+    print('The number is 201!!!')
+
+if '11001010' == num:
+    print('The number is 202!!!')
+
+if '11001011' == num:
+    print('The number is 203!!!')
+
+if '11001100' == num:
+    print('The number is 204!!!')
+
+if '11001101' == num:
+    print('The number is 205!!!')
+
+if '11001110' == num:
+    print('The number is 206!!!')
+
+if '11001111' == num:
+    print('The number is 207!!!')
+
+if '11010000' == num:
+    print('The number is 208!!!')
+
+if '11010001' == num:
+    print('The number is 209!!!')
+
+if '11010010' == num:
+    print('The number is 210!!!')
+
+if '11010011' == num:
+    print('The number is 211!!!')
+
+if '11010100' == num:
+    print('The number is 212!!!')
+
+if '11010101' == num:
+    print('The number is 213!!!')
+
+if '11010110' == num:
+    print('The number is 214!!!')
+
+if '11010111' == num:
+    print('The number is 215!!!')
+
+if '11011000' == num:
+    print('The number is 216!!!')
+
+if '11011001' == num:
+    print('The number is 217!!!')
+
+if '11011010' == num:
+    print('The number is 218!!!')
+
+if '11011011' == num:
+    print('The number is 219!!!')
+
+if '11011100' == num:
+    print('The number is 220!!!')
+
+if '11011101' == num:
+    print('The number is 221!!!')
+
+if '11011110' == num:
+    print('The number is 222!!!')
+
+if '11011111' == num:
+    print('The number is 223!!!')
+
+if '11100000' == num:
+    print('The number is 224!!!')
+
+if '11100001' == num:
+    print('The number is 225!!!')
+
+if '11100010' == num:
+    print('The number is 226!!!')
+
+if '11100011' == num:
+    print('The number is 227!!!')
+
+if '11100100' == num:
+    print('The number is 228!!!')
+
+if '11100101' == num:
+    print('The number is 229!!!')
+
+if '11100110' == num:
+    print('The number is 230!!!')
+
+if '11100111' == num:
+    print('The number is 231!!!')
+
+if '11101000' == num:
+    print('The number is 232!!!')
+
+if '11101001' == num:
+    print('The number is 233!!!')
+
+if '11101010' == num:
+    print('The number is 234!!!')
+
+if '11101011' == num:
+    print('The number is 235!!!')
+
+if '11101100' == num:
+    print('The number is 236!!!')
+
+if '11101101' == num:
+    print('The number is 237!!!')
+
+if '11101110' == num:
+    print('The number is 238!!!')
+
+if '11101111' == num:
+    print('The number is 239!!!')
+
+if '11110000' == num:
+    print('The number is 240!!!')
+
+if '11110001' == num:
+    print('The number is 241!!!')
+
+if '11110010' == num:
+    print('The number is 242!!!')
+
+if '11110011' == num:
+    print('The number is 243!!!')
+
+if '11110100' == num:
+    print('The number is 244!!!')
+
+if '11110101' == num:
+    print('The number is 245!!!')
+
+if '11110110' == num:
+    print('The number is 246!!!')
+
+if '11110111' == num:
+    print('The number is 247!!!')
+
+if '11111000' == num:
+    print('The number is 248!!!')
+
+if '11111001' == num:
+    print('The number is 249!!!')
+
+if '11111010' == num:
+    print('The number is 250!!!')
+
+if '11111011' == num:
+    print('The number is 251!!!')
+
+if '11111100' == num:
+    print('The number is 252!!!')
+
+if '11111101' == num:
+    print('The number is 253!!!')
+
+if '11111110' == num:
+    print('The number is 254!!!')
+
+if '11111111' == num:
+    print('The number is 255!!!')
+
+print('Thanks for using my calculator!!!!')
