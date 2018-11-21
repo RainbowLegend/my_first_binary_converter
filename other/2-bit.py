c260eae5293bfa5e24082b9527b44052b992a335
RainbowLegend <rainbowlegend3054@gmail.com>
2018-11-20 22:43:24 -0500

    2 BITS!!!!

---
+++ other/2-bit.py
@@ -0,0 +1,16 @@
+print('Welcome to my first converter!!!!')
+num = input('Whats your number???')
+
+if '00' == num:
+    print('The number is 0!!!')
+
+if '01' == num:
+    print('The number is 1!!!')
+
+if '10' == num:
+    print('The number is 2!!!')
+
+if '11' == num:
+    print('The number is 3!!!')
+
+print('Thanks for using my calculator!!!!')
