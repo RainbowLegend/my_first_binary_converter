63726f683c2add1cb0ee80b66269e6ba47f87f6d
RainbowLegend <rainbowlegend3054@gmail.com>
2018-11-20 21:14:02 -0500

    This was a fun project

---
+++ generator.py
@@ -0,0 +1,40 @@
+from itertools import combinations
+
+iters = int(input('How many binary digits shall there be?\n'))
+bin_nums = []
+
+for i in range(iters):
+    bin_nums.append("0")
+    bin_nums.append("1")
+
+statements = []
+
+for i in range(iters):
+    for combo in combinations(bin_nums, i+1): statements.append(combo)
+
+f = open('my_first_converter.py', 'w+')
+
+to_compile = ""
+
+to_compile += "print('Welcome to my first converter!!!!')\n"
+to_compile += "num = input('Whats your number???')\n\n"
+
+binaries = sorted(set([('0' * (iters - len(i))) + ''.join(i) for i in [''.join(i) for i in statements]]))
+
+print(binaries)
+for i in binaries:
+    to_compile += f"if '{i}' == num:\n"
+
+    fin_num = 0
+
+    for pos, value in enumerate(i[::-1]):
+        fin_num += (int(value) * 2) ** pos
+
+    fn = (fin_num if i[-1] == '1' else fin_num - 1)
+    to_compile += f"    print('The number is {fn}!!!')\n\n"
+
+to_compile += "print('Thanks for using my calculator!!!!')\n"
+
+f.write(to_compile)
+f.close()
+print('Your converter is done!')
