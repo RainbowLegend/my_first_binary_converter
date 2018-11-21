bc47729ee81774d39fc430dbaf4e318152b52488
RainbowLegend <rainbowlegend3054@gmail.com>
2018-11-20 22:43:44 -0500

    4 BITS!!!!

---
+++ other/4-bit.py
@@ -0,0 +1,52 @@
+print('Welcome to my first converter!!!!')
+num = input('Whats your number???')
+
+if '0000' == num:
+    print('The number is 0!!!')
+
+if '0001' == num:
+    print('The number is 1!!!')
+
+if '0010' == num:
+    print('The number is 2!!!')
+
+if '0011' == num:
+    print('The number is 3!!!')
+
+if '0100' == num:
+    print('The number is 4!!!')
+
+if '0101' == num:
+    print('The number is 5!!!')
+
+if '0110' == num:
+    print('The number is 6!!!')
+
+if '0111' == num:
+    print('The number is 7!!!')
+
+if '1000' == num:
+    print('The number is 8!!!')
+
+if '1001' == num:
+    print('The number is 9!!!')
+
+if '1010' == num:
+    print('The number is 10!!!')
+
+if '1011' == num:
+    print('The number is 11!!!')
+
+if '1100' == num:
+    print('The number is 12!!!')
+
+if '1101' == num:
+    print('The number is 13!!!')
+
+if '1110' == num:
+    print('The number is 14!!!')
+
+if '1111' == num:
+    print('The number is 15!!!')
+
+print('Thanks for using my calculator!!!!')
